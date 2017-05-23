b5c8cdc792287bbb390469200c6d07352c39db92
Dylan Burton <dylanjburton2@gmail.com>
2017-05-23 00:14:34 +0000

    Fix migration and migrate

--- db/migrate/20170522231405_create_users.rb
+++ db/migrate/20170522231405_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.camosun_id :string, null:false, limit: 8
-      t.role :integer, null:false
-      t.first_name :string, limit: 20
-      t.last_name :string, limit: 20
+      t.string :camosun_id, null:false, limit: 8
+      t.integer :role, null:false
+      t.string :first_name, limit: 20
+      t.string :last_name, limit: 20
       t.timestamps
     end
     add_index :users, :camosun_id, unique: true
