add23f5414f814dfb8588481562f4b7426f6c93a
Dylan Burton <dylanjburton2@gmail.com>
2017-05-23 08:47:00 +0000

    Change user and request association to has_one

--- db/migrate/20170523080511_create_requests.rb
+++ db/migrate/20170523080511_create_requests.rb
@@ -1,9 +1,9 @@
 class CreateRequests < ActiveRecord::Migration[5.0]
   def change
     create_table :requests do |t|
+      t.belongs_to :user, index: {unique: true}
       t.integer :type, null:false
       t.text :info
-      t.belongs_to :user, index: true
       t.timestamps
     end
   end
