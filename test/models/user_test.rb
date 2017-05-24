895af6133f5ab9314d792447cd980bb12eb0fafa
Dylan Burton <dylanjburton2@gmail.com>
2017-05-24 01:58:35 +0000

    Add test. Model association broken.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @instructor = User.new(camosun_id: "C1234567", role: 0, 
+      first_name: "Example", last_name: "Instructor")
+    @student = User.new(camosun_id: "C8765432", role: 1, 
+      first_name: "Example", last_name: "Student")
+  end
+  
+  test "instructor should be valid" do 
+    assert @instructor.valid?
+  end
+  
+  test "Student should be valid" do
+    assert @student.valid?
+  end
+  
+  
 end
