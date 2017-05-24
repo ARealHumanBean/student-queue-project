f997739935c06df6c3adbe74a7a2d591a3bc3a65
Dylan Burton <dylanjburton2@gmail.com>
2017-05-24 00:38:40 +0000

    add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_one :request
+  
+  validates :camosun_id, presence: true, length: { maximum: 8 }, 
+    uniqueness: true
+  validates :role, presence: true
+  validates :first_name, length: { maximum: 20 }
+  validates :last_name, length: { maximum: 20 }
 end
