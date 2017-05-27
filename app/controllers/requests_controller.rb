7f64b9a562f4034c9cfea4ee1e7bc8d1511ee67a
Dylan Burton <dylanjburton2@gmail.com>
2017-05-27 21:35:46 +0000

    Add seed file with user and request data

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,21 @@
 class RequestsController < ApplicationController
+  def new 
+  end
+  
+  # Manage Requests view
+  def index
+    @requests = Request.all
+    @question = Request.question.all
+    @demo = Request.demo.all
+    @support = Request.support.all
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+  
+  def edit  
+  end
 end
