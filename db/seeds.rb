604c2a68df64e5d36b93aab83dc25ae946e360d5
Dylan Burton <dylanjburton2@gmail.com>
2017-05-27 23:17:34 +0000

    Remove unnecessary commas in user_list

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user_list = [
-  ["C0000001", :instructor, "joe", "biden", ],
-  ["C0000002", :student, "chris", "lastname", ],
-  ["C0000003", :student, "firstname", "", ],
-  ["C0000004", :student, "", "lastname", ],
-  ["C0000005", :student, "firstname", "lastname", ],
-  ["C0000006", :student, "man", "dood", ],
-  ["C0000007", :student, "sad", "boy", ],
-  ["C0000008", :student, "derg", "hiut", ],
-  ["C0000009", :student, "aksr", "lop", ],
-  ["C0000010", :student, "rateq", "lokm", ]]
+  ["C0000001", :instructor, "joe", "biden"],
+  ["C0000002", :student, "chris", "lastname"],
+  ["C0000003", :student, "firstname", ""],
+  ["C0000004", :student, "", "lastname"],
+  ["C0000005", :student, "firstname", "lastname"],
+  ["C0000006", :student, "man", "dood"],
+  ["C0000007", :student, "sad", "boy"],
+  ["C0000008", :student, "derg", "hiut"],
+  ["C0000009", :student, "aksr", "lop"],
+  ["C0000010", :student, "rateq", "lokm"]]
 
 queue_types = [:question, :demo, :support]
 
