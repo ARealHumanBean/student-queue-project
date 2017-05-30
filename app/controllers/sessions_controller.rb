d8201b9f33d31265b1c450d933d07bb2bbe3ba29
Dylan Burton <dylanjburton2@gmail.com>
2017-05-30 08:20:22 +0000

    Add login page (doesn't do anything yet)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    flash.now[:danger] = "I'm in here"
+    user = User.find_by(camosun_id: params[:session][:camosun_id].upcase)
+    if user
+      # Log the user in and display their respective views.
+      flash.now[:danger] = "logged in"
+    else
+      # create an error message
+      flash.now[:danger] = "invalid email/password combination"
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
 end
