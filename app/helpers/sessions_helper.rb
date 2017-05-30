c7d60e79040c2ea41d0c5c45e8dbbc18021a5307
Dylan Burton <dylanjburton2@gmail.com>
2017-05-30 09:39:18 +0000

    Add Delete method, and a show method for user.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # Log out a user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
   # Return the current logged-in user
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
