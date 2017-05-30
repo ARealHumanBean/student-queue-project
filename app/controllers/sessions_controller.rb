906ac59e1161bdc68893cdecc9650c51f34124f9
Dylan Burton <dylanjburton2@gmail.com>
2017-05-30 08:28:15 +0000

    Remove unnecessary flashes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,9 @@ class SessionsController < ApplicationController
   end
   
   def create
-    flash.now[:danger] = "I'm in here"
     user = User.find_by(camosun_id: params[:session][:camosun_id].upcase)
     if user
       # Log the user in and display their respective views.
-      flash.now[:danger] = "logged in"
     else
       # create an error message
       flash.now[:danger] = "invalid email/password combination"
