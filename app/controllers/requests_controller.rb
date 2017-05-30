eb72ee3111ec49489d2121c4b188fa1c3f4797c9
Dylan Burton <dylanjburton2@gmail.com>
2017-05-30 13:05:54 +0000

    Create request functionality created

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,6 @@
 class RequestsController < ApplicationController
   def new 
+    @request = Request.new
   end
   
   # Manage Requests view
@@ -28,8 +29,18 @@ class RequestsController < ApplicationController
     end
     
     # redirect to the selected queue in the manage requests view
-    redirect_to requests_url(queue_type: "#{request.queue_type}")
+    redirect_to requests_url(queue_type: request.queue_type)
     flash[:success] = "Request Completed"
+  end
+  
+  def create 
+    @request = Request.new(request_params)
+    @request.user_id = current_user.id
+    if @request.save
+      redirect_to @request
+    else
+      render 'new'
+    end
   end
   
   def edit  
@@ -37,6 +48,6 @@ class RequestsController < ApplicationController
   
   private
     def request_params
-      params.require(:request).permit(:queue_type, :id)
+      params.require(:request).permit(:queue_type, :info)
     end
 end
