8ca3429a151cde570bf8018b0cffe8d575448d30
Dylan Burton <dylanjburton2@gmail.com>
2017-06-01 06:25:45 +0000

    Check for logged in users on all pages by default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
   include SessionsHelper
+  
+  # before any controller action, check to make sure a user is logged in. 
+  before_action :require_login
+  
+  private
+  
+  # redirect to login page if user isn't logged in
+  def require_login
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
