969f5d317e286a33b593dfe7fa6ce300920b02fb
Dylan Burton <dylanjburton2@gmail.com>
2017-06-02 21:04:41 +0000

    Changes routes for users and request index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  get '/manage_requests', to: 'requests#index'
+  get '/manage_users', to: 'users#index'
   resources :users, :requests
   
 end
