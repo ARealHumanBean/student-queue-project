993790103bbb1b76c75508d62f4f7c14c62882a5
Dylan Burton <dylanjburton2@gmail.com>
2017-06-09 23:59:49 +0000

    Fix create users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ require 'csv'
 class UsersController < ApplicationController
   before_action :require_instructor, :except=>[:show]
   def new
+    @user = User.new
   end
   
   def index
   end
   
   def create
+    class_list = params[:user][:class_list]
     CSV.foreach(class_list, :headers => true) do |row|
       User.create!(row.to_hash)
     end
