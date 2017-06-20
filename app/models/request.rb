ff5fe53d9de61bcb5925d1daa586dda145ff7442
Dylan Burton <dylanjburton2@gmail.com>
2017-06-20 05:13:08 +0000

    Add spacing for request partial

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,7 +2,7 @@ class Request < ApplicationRecord
   belongs_to :user
   
   enum queue_type: {question: 0, support: 1, demo: 2}
-  # Set type to quick_question by default
+  # Set type to question by default
   after_initialize :set_default_type, :if => :new_record?
   
   validates :queue_type, presence: true
