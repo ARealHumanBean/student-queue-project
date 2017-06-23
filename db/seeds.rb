b3c3189a2acefcee2fde776d645e66146adb161f
Dylan Burton <dylanjburton2@gmail.com>
2017-06-23 18:18:48 +0000

    Remove user's in seed except  initial instructor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 user_list = [
-  ["C0000001", :instructor, "joe", "biden"],
-  ["C0000002", :student, "chris", "lastname"],
-  ["C0000003", :student, "don", "atello"],
-  ["C0000004", :student, "Jon", "Ilre"],
-  ["C0000005", :student, "Loke", "Jhequ"],
-  ["C0000006", :student, "Daniel", "loken"],
-  ["C0000007", :student, "Nick", "Lammy"],
-  ["C0000008", :student, "Alex", "Beinet"],
-  ["C0000009", :student, "hone", "lope"],
-  ["C0000010", :student, "ques", "Ipe"],
-  ["C0000011", :student, "Takumi", "Takumi"],
-  ["C0000012", :student, "Dylan", "Burton"],
-  ["C0000013", :student, "Jack", "Beanstalk"],
-  ["C0000014", :student, "Loser", "Dude"]]
+  ["C0000001", :instructor, "Instructor", "Account"]]
 
 queue_types = [:question, :demo, :support]
 
